80d99a9d4f75a0d73a0725df88006cb2891ec5e5
timsonner <66705347+timsonner@users.noreply.github.com>
2022-11-07 14:10:28 -0700

    link ui slider to view background color - red of rgba

--- Color Table View/Color Table View/ColorsDetailsVC.swift
+++ Color Table View/Color Table View/ColorsDetailsVC.swift
@@ -11,18 +11,26 @@ class ColorsDetailsVC: UIViewController {
     
     var color: UIColor?
 
+
     @IBOutlet weak var redLabel: UILabel!
     
     @IBAction func redSlider(_ sender: UISlider) {
         redLabel.text = String(sender.value)
+        let colorAttributes = color?.getRGBValues(color: color!)
+        let updatedColor = color?.setRGBValues(color: color!, red: CGFloat(sender.value), green: colorAttributes!.floatGreen, blue: colorAttributes!.floatBlue, alpha: colorAttributes!.floatAlpha)
+        view.backgroundColor = updatedColor
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Data was recieved from the segue.
+        // Set view background color
         view.backgroundColor = color ?? .white
-        color?.getRGBValues(color: color!)
+
+        // MARK: Get attributes of color
+        // Initialize slider with value of color attribute
+        let colorAttributes = color?.getRGBValues(color: color!)
+        redLabel.text = "\(colorAttributes!.floatRed)"
     }
     
 }
